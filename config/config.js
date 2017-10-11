68aaa8708c035a876ddf9c5fd59d20399598b28f
Thomas Chan <thomas.chan@hubio.com>
2017-10-11 19:30:31 -0400

    Added local dynamodb instance configurations.

--- config/config.js
+++ config/config.js
@@ -4,5 +4,6 @@ var db = require('dynamoose');
 
 // Use environment variable STAGE set from serverless.yml
 module.exports.DEPLOYMENT_STAGE = process.env.STAGE || 'default';
-module.exports.GRAPHQL_ENDPOINT = process.env.GRAPHQL_ENDPOINT || '/graphql'
+module.exports.GRAPHQL_ENDPOINT = process.env.GRAPHQL_ENDPOINT || ''
 module.exports.DB = db;
+module.exports.DYNAMODB_ENV = 'local';
--- config/utils/init.js
+++ config/utils/init.js
@@ -6,6 +6,12 @@
     module.exports.initConfig = function () {
         // Set database table prefix to deployment_stage name
         config.DB.setDefaults({ prefix: config.DEPLOYMENT_STAGE + "_" });
+        if (config.DYNAMODB_ENV === 'local'){
+            config.DB.local('http://localhost:8000')
+            config.DB.AWS.config.update({
+                region: 'localhost'
+            })            
+        }
     }
 
     module.exports.getConfig = function(){
