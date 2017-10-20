eac11f5e8fc5c49b8ba3701bcea7b9f1af5cc3df
Thomas Chan <thomas.chan@hubio.com>
2017-10-19 23:02:56 -0400

    Fix paws GET and POST responses.

--- components/paws/paws.GET.js
+++ components/paws/paws.GET.js
@@ -13,19 +13,21 @@ var pawsGET = function (event, context, callback) {
         if(err){
             response = {
                 statusCode: 500,
-                body: {
+                body: JSON.stringify({
                     message: 'Error Getting Paw with dogId: ' + dogId,
                     output: err
-                }
-            };            
+                })
+            };
+            console.log('Error!',response)               
         } else {
             response = {
                 statusCode: 200,
-                body: {
+                body: JSON.stringify({
                     message: 'Success GET on dogId: ' + dogId,
                     output: dogs
-                }
-            };            
+                })
+            };         
+            console.log('Success!',response)   
         }        
         callback(null, response);
     })
--- components/paws/paws.POST.js
+++ components/paws/paws.POST.js
@@ -8,23 +8,24 @@ var pawsPOST = function (event, context, callback) {
     var body = JSON.parse(event.body);
     var request = new paw(body);
     request.save(function (err) {
-        if (err) {
-            console.log('Save Error: ', err);
+        if (err) {            
             response = {
                 statusCode: 500,
-                body: {
+                body: JSON.stringify({
                     message: err,
                     input: body
-                }
+                })
             }
-        } else {
+            console.log('Save Error: ', response);
+        } else {            
             response = {
                 statusCode: 200,
-                body: {
+                body: JSON.stringify({
                     message: 'Saved Paw!',
                     input: body
-                }
+                })
             };
+            console.log('Saved Paw!',response);            
         }
         callback(null, response);
     })
