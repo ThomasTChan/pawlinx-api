7979a829a181c08600205b6f28f0a2d2f75599f5
Thomas Chan <thomas.chan@hubio.com>
2017-11-12 16:18:11 -0500

    Added GET for owners

--- components/owners/owners.GET.js
+++ components/owners/owners.GET.js
@@ -4,29 +4,48 @@ var dynamoose = require('dynamoose'),
     owner = require('./models/owners').model;
 
 var ownersGET = function (event, context, callback) {
-    var response = {};
-    var request = new owner(JSON.parse(event.body));
-    request.save(function (err) {
-        if (err) {
-            console.log('Save Error: ', err);
-            response = {
-                statusCode: 500,
-                body: JSON.stringify({
-                    message: err,
-                    input: event.body
-                })
+    var response = {},
+        cognitoIdentityId = event.requestContext.identity.cognitoIdentityId,
+        cognitoIdentityPoolId = event.requestContext.identity.cognitoIdentityPoolId;
+
+    context.util.getAWSCognitoIdentityRecord(cognitoIdentityId, cognitoIdentityPoolId).then(function (data) {
+        owner.query({
+            accountId: {
+                eq: data.accountId
             }
-        } else {
-            response = {
-                statusCode: 200,
-                body: JSON.stringify({
-                    message: 'Saved Owner!',
-                    input: event.body,
-                }),
-            };
-        }
+        }, function (err, owners) {
+            if (err) {
+                response = {
+                    statusCode: 500,
+                    body: JSON.stringify({
+                        message: 'Error Getting Paws for account: ' + data.accountId,
+                        output: err
+                    })
+                };
+                console.log('Error!', response)
+            } else {
+                response = {
+                    statusCode: 200,
+                    body: JSON.stringify({
+                        message: 'Success GET Paws for account: ' + data.accountId,
+                        output: owners
+                    })
+                };
+                console.log('Success!', response)
+            }
+            callback(null, response);
+        })
+    }, function (error) {
+        console.log(error);
+        response = {
+            statusCode: 500,
+            body: JSON.stringify({
+                message: 'Cognito Identity Error: ' + error.msg,
+                errors: error.errors
+            })
+        };
         callback(null, response);
-    })    
+    })  
 };
 
 module.exports = ownersGET;
