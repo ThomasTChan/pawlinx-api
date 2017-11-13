882294a1b83db5101123d709a568d4c6a562cf7a
Thomas Chan <thomas.chan@hubio.com>
2017-11-12 23:47:20 -0500

    Added DELETE method for paws API

---
+++ components/paws/paws.DELETE.js
@@ -0,0 +1,78 @@
+'use strict';
+
+var paw = require('./models/paws').model;
+
+var pawsPUT = function (event, context, callback) {
+    var response = {},
+        AWS = context.AWS,
+        cognitoIdentityId = event.requestContext.identity.cognitoIdentityId,
+        cognitoIdentityPoolId = event.requestContext.identity.cognitoIdentityPoolId;
+
+    // Check if id belongs to cognito identity in context
+    context.util.getAWSCognitoIdentityRecord(cognitoIdentityId, cognitoIdentityPoolId).then(function (data) {
+        paw.queryOne('accountId').eq(data.accountId)
+            .where('pawId')
+            .eq(event.pathParameters.id)
+            .exec(function (err, found_paw) {
+                // if err then pawId does not belong to cognito identity in context
+                if (err) {
+                    response = {
+                        statusCode: 500,
+                        body: JSON.stringify({
+                            message: err,
+                            input: event.pathParameters.id
+                        })
+                    }
+                    callback(null, response);
+                } else if (typeof found_paw === 'undefined') {
+                    // Resrouce does not exist!
+                    response = {
+                        statusCode: 404,
+                        body: JSON.stringify({
+                            message: 'DELETE: ' + event.pathParameters.id + ' failed as resource does not exist!',
+                            output: event.pathParameters.id + ' does not exist for logged in account.'
+                        })
+                    }
+                    callback(null, response);
+                } else {
+                    found_paw.delete(function (del_err) {
+                        if (del_err) {
+                            response = {
+                                statusCode: 500,
+                                body: JSON.stringify({
+                                    message: del_err,
+                                    input: event.pathParameters.id
+                                })
+                            }
+                            callback(null, response);
+                        } else {
+                            // Delete successful!
+                            response = {
+                                statusCode: 200,
+                                body: JSON.stringify({
+                                    message: 'DELETE: ' + event.pathParameters.id + ' Successful!',
+                                    output: found_paw
+                                })
+                            }
+                            callback(null, response);
+                        }
+                    })
+
+                }
+            })
+       
+    }, function (error) {
+        console.log(error);
+        response = {
+            statusCode: 500,
+            body: JSON.stringify({
+                message: 'Cognito Identity Error: ' + error.msg,
+                errors: error.errors,
+                input: event.pathParameters.id
+            })
+        };
+        callback(null, response);
+    })
+}
+
+module.exports = pawsPUT;
