902d5830dd4968ebc82226b0fedf319329d6b674
Thomas Chan <thomas.chan@hubio.com>
2017-11-14 11:33:21 -0500

    each account should only have one owner id

--- components/owners/owners.POST.js
+++ components/owners/owners.POST.js
@@ -2,7 +2,29 @@
 
 var dynamoose = require('dynamoose'),
     uuid = require('uuid'),
-    owner = require('./models/owners').model;
+    owner = require('./models/owners').model,
+    Q = require('q');
+
+function getOwnerId(accountId) {
+    var deferred = Q.defer(),
+        promise = deferred.promise;
+
+    owner.query('accountId').eq(accountId)
+        .attributes(['ownerId'])
+        .exec(function (err, owners) {
+            if (err) {
+                deferred.reject(err);
+            } else if (owners.length === 0) {
+                // Resource does not exist, create new UUID
+                deferred.resolve(uuid());
+            } else {
+                // Resource exists, return UUID
+                deferred.resolve(owners[0].ownerId);
+            }
+        })
+
+    return promise;
+}
 
 var ownersPOST = function (event, context, callback) {
     var response = {},
@@ -13,30 +35,42 @@ var ownersPOST = function (event, context, callback) {
         cognitoIdentityPoolId = event.requestContext.identity.cognitoIdentityPoolId;
 
     context.util.getAWSCognitoIdentityRecord(cognitoIdentityId, cognitoIdentityPoolId).then(function (data) {
-        request.accountId = data.accountId;
-        request.beaconId = data.beaconId;
-        request.ownerId = uuid();
-        request.save(function (err) {
-            if (err) {
-                console.log('Save Error: ', err);
-                response = {
-                    statusCode: 500,
-                    body: JSON.stringify({
-                        message: 'POST failed!',
-                        input: body,
-                        output: err
-                    })
+        getOwnerId(data.accountId).then(function (owner_uuid) {
+            request.accountId = data.accountId;
+            request.beaconId = data.beaconId;
+            request.ownerId = owner_uuid;
+            request.save(function (err) {
+                if (err) {
+                    console.log('Save Error: ', err);
+                    response = {
+                        statusCode: 500,
+                        body: JSON.stringify({
+                            message: 'POST failed!',
+                            input: body,
+                            output: err
+                        })
+                    }
+                } else {
+                    response = {
+                        statusCode: 200,
+                        body: JSON.stringify({
+                            message: 'POST successful!',
+                            input: body,
+                            output: request
+                        }),
+                    };
                 }
-            } else {
-                response = {
-                    statusCode: 200,
-                    body: JSON.stringify({
-                        message: 'POST successful!',
-                        input: body,
-                        output: request
-                    }),
-                };
-            }
+                callback(null, response);
+            })
+        }, function (getOwnerError) {
+            response = {
+                statusCode: 200,
+                body: JSON.stringify({
+                    message: 'POST failed!',
+                    input: body,
+                    output: getOwnerError
+                }),
+            };
             callback(null, response);
         })
     }, function (error) {
